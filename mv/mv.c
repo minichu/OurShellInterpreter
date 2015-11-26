88c91f4a2ab9c8c83e40e1d246c77fa28168ddec
Jiwon Seok <minichuuuuu@gmail.com>
2015-11-26 07:28:22 -0800

    mvtest

---
+++ mv/mv.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+	int fd_des, fd_src;
+	char buf;
+
+	if(argc != 3){
+		printf("error : Invalid format.\n");
+	}
+
+	if((fd_src = open(argv[1], O_RDONLY)) < 0) {
+		printf("error : open fail");
+		exit(0);
+	}
+	
+	rename(argv[1], argv[2]);
+	return 0;
+}
