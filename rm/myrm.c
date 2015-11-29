d5c455b0cbbd7d02ebbc96586353309464f45796
LeeHeeSeok <hr5177@naver.com>
2015-11-29 12:04:44 +0900

    make myrm and test complete

--- rm/myrm.c
+++ rm/myrm.c
@@ -5,6 +5,18 @@
 
 int main(int argc, char *argv[])
 {
-	if
+	if(argc!=2)
+	{
+		printf("ERROR: format (./myrm<filename>).\n");
+		exit(1);
+	}
 
+	if(open(argv[1],O_RDWR)<0)
+	{
+		printf("There is no %s file.\n",argv[1]);
+	}
+	if(unlink(argv[1])<0)
+		printf("ERROR: Unlink Failed.\n");
+	else
+		printf("Delete success.\n");
 }
