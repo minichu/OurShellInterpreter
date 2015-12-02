f3143b5002a50962b6bdcd627a503b92a42117a9
HeeSeok Lee <hr5177@naver.com>
2015-12-02 00:15:41 -0800

    add getcommand func

--- main.c
+++ main.c
@@ -1,8 +1,135 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #define MAXSIZE		2000
+
+
+
+int getcommand(char *command)
+{
+	int n=0, space_chk=0;
+	char path[MAXSIZE] = {0};
+	char c;
+	nowhistory = histail;
+	
+	fflush(NULL);
+
+	// 프롬프트를 띄우고 명령 입력을 대기
+	curpath = (char *)getenv("PWD");
+	printf("[%s@%s]$ ", userinfo->pw_name, curpath);
+	sprintf(path, "[%s@%s]$ ", userinfo->pw_name, curpath);
+
+	// 명령 입력을 받음
+	while((c = fgetc(stdin)))
+	{
+		// enter key를 입력받으면 입력 종료
+		if(c == 10)
+		{
+			fputc('\n', stdout);
+			break;
+		}
+
+		// Ctrl+D를 입력받으면 입력 종료
+		if(c == 4)
+		{
+			fputc('\n', stdout);
+			return 0;
+		}
+		
+		// backspace key를 제외한 입력 무시
+		if((c <= 26) && (c != 8))
+			break;
+			
+		switch(c)
+		{
+			// backspace key를 입력받을 때 처리
+			case 8:
+				if(n == 0)
+					break;
+
+				fputc('\b', stdout);
+				fputc(' ', stdout);
+				fputc('\b', stdout);
+				command[--n] = (char)0;
+				break;
+	
+			// ^[ (esc) 기호이면서
+			case 27:
+				// [ 기호가 아니면 switch문 탈출
+				if((c = fgetc(stdin)) != 91)
+					break;
+
+				// [ 기호가 들어왔다면
+				switch(fgetc(stdin))
+				{
+					// ^[[A 기호, 즉 key가 up일 때 history 내용 출력
+					case 65:
+						nowhistory = nowhistory->prev;
+						if(nowhistory != hishead)
+						{
+							fprintf(stdout, "\r%80s", " ");
+							fprintf(stdout, "\r%s%s", path,
+							 nowhistory->value);
+							strcpy(command,
+							 nowhistory->value);
+							n = strlen(command);
+						}
+						else
+						{
+							nowhistory = hishead->next;
+						}
+						break;
+					// ^[[B 기호, 즉 key가 down일 때 history 내용 출력
+					case 66:
+						nowhistory = nowhistory->next;
+						if(nowhistory != histail)
+						{
+							fprintf(stdout, "\r%80s", " ");
+							fprintf(stdout, "\r%s%s", path,
+							 nowhistory->value);
+							strcpy(command,
+							 nowhistory->value);
+							n = strlen(command);
+						}
+						else
+						{
+							fprintf(stdout, "\r%80s", " ");
+							fprintf(stdout, "\r%s", path);
+							n = 0;
+						}
+						break;
+					// ^[[C 기호, 즉 key가 right일 때 아무 처리 안함
+					case 67:
+						break;
+					// ^[[D 기호, 즉 key가 left일 때 아무 처리 안함
+					case 68:
+						break;
+				}
+				break;
+		
+			// 그 외 일반적인 문자이면
+			default:
+				fputc(c, stdout);
+				command[n++] = (char)c;
+				break;
+		}
+	}
+	command[n] = '\0';
+
+	// 아무런 입력이 없었으면 프롬프트 다시 띄움
+	if(strlen(command) == 0)
+		return 0;
+	
+	// 공백만 포함된 문자열이면 프롬프트 다시 띄움
+	for(n=0; n<strlen(command); n++)
+		if(command[n] != ' ' && command[n] != '\t')
+			return 1;
+	
+	return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -22,8 +149,8 @@ int main(int argc, char *argv[])
 
 	if(argc != 1)
 	{
-		perror("usage : mysh");
-		exit(-1);
+		perror("usage : Our Shell Interpreter");
+		exit(errno);
 	}	
 
 	while(1)
