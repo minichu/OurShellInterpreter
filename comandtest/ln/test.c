4e25d0355b6140ad116dded01d69bf8e7f3f627d
Jiwon Seok <minichuuuuu@gmail.com>
2015-12-08 19:00:30 -0800

    last commit

---
+++ comandtest/ln/test.c
@@ -0,0 +1,19 @@
+#include "myln.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc!=3)
+	{
+		printf("ERROR: format(./myln <source> <destination>).\n");
+		exit(1);
+	}
+	if(open(argv[1],O_RDWR) < 0)
+	{
+		printf("There is no %s file.\n",argv[1]);
+		exit(1);
+	}
+	if(link(argv[1],argv[2])<0)
+		printf("link error.\n");
+	else
+		printf("link success!!\n");
+}
